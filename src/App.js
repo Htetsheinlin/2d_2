2b6d240badf113532c3ec55b8ecc8824af32a5a9
Htetsheinlin <44019910+Htetsheinlin@users.noreply.github.com>
2023-09-21 20:39:22 +0630

    First commt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [a, setA] = useState('');
+  const [b, setB] = useState('');
+  const [c, setC] = useState('1'); // Default value for c is '1'
+  const [result, setResult] = useState('');
+
+  const handleCalculate = () => {
+    // Parse input values as floats
+    const aFloat = parseFloat(a);
+    const bFloat = parseFloat(b);
+    const cFloat = parseFloat(c);
+
+    // Check if input values are valid numbers
+    if (isNaN(aFloat) || isNaN(bFloat) || isNaN(cFloat)) {
+      setResult('Please enter valid numbers');
+    } else {
+      // Perform the calculation
+      const calculatedResult = ((aFloat - (aFloat * 0.18)) - (bFloat * 80)) * (cFloat / 100);
+      setResult(`Result: ${calculatedResult.toFixed(2)}`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <div>
+        <label htmlFor="a">Enter total sell price</label>
+        <br></br>
+        <input type="text" id="a" value={a} onChange={(e) => setA(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="b">Enter total pauk thee</label>
+        <br></br>
+        <input type="text" id="b" value={b} onChange={(e) => setB(e.target.value)} />
+      </div>
+      <div>
+        <label htmlFor="c">Select your percentage</label>
+        <br></br>
+        <select id="c" value={c} onChange={(e) => setC(e.target.value)}>
+          <option value="1">1</option>
+          <option value="1.25">1.25</option>
+          <option value="1.5">1.5</option>
+          <option value="2">2</option>
+          <option value="2.5">2.5</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="12.5">12.5</option>
+        </select>
+      </div>
+      <br></br>
+      <button onClick={handleCalculate}>$</button>
+      <br></br>
+      <div>{result}</div>
     </div>
   );
 }
